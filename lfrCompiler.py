35ee9f4de276f90713a28cc6211bd3ea1e413774
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2019-12-11 00:03:49 -0500

    Made changes for graph coverage example to work again

--- lfrCompiler.py
+++ lfrCompiler.py
@@ -327,198 +327,20 @@ class LFRCompiler(lfrXListener):
             # Make 1-1 connections
             for source, target in zip(rhs, lhs):
                 print(source, target)
-                if isinstance(source, ModuleIO):
-                    sourceid = source.name
-                else:
-                    sourceid = source.id
-
-                if isinstance(target, ModuleIO):
-                    targetid = target.id
-                else:
-                    targetid = target.id
+                sourceid = source.id
+                targetid = target.id
 
                 self.currentModule.add_fluid_connection(sourceid, targetid)
 
         elif len(lhs) != len(rhs):
             print("LHS not equal to RHS")
             for source in rhs:
-                if isinstance(source, ModuleIO):
-                    sourceid = source.name
-                else:
-                    sourceid = source.id
+                sourceid = source.id
 
                 for target in lhs:
-                    if isinstance(target, ModuleIO):
-                        targetid = target.name
-                    else:
-                        targetid = target.id
-
+                    targetid = target.id
                     self.currentModule.add_fluid_connection(sourceid, targetid)
 
-
-
-    
-
-
-
-           
-
-
-
-
-    # def enterLhs(self, ctx: lfrXParser.LhsContext):
-    #     self.listermode = ListenerMode.LHS_PARSING_MODE
-
-    # def exitLhs(self, ctx: lfrXParser.LhsContext):
-    #     self.listermode = ListenerMode.FLUID_ASSIGN_STAT_MODE
-
-    # def exitVectorvar(self, ctx: lfrXParser.VectorvarContext):
-
-    #     name = ctx.ID().getText()
-    #     if name not in self.vectors:
-    #         self.compilingErrors.append(
-    #             LFRError(ErrorType.VARIABLE_NOT_RECOGNIZED,
-    #                      "Variable:{0} was not recognized, check to see if this was declared earlier".format(name)))
-    #         return
-
-    #     vec = self.vectors[name]
-
-    #     startindex = 0
-    #     endindex = 0
-
-    #     if ctx.vector() is not None:
-    #         startindex = int(ctx.vector().start.text)
-    #         endindex = int(ctx.vector().end.text)
-
-    #     vrange = VectorRange(vec, startindex, endindex)
-
-    #     #Sets the current temp store
-    #     self.currentvectorranges.append()
-
-    #     #Adds to the stack
-    #     self.currentvectorranges.append(vrange)
-
-    #     if self.listermode is ListenerMode.LHS_PARSING_MODE:
-    #         self.lhs = vrange
-    #     else:
-    #         if self.expressionvariablestack is not None:
-    #             self.expressionvariablestack.append(vrange)
-    #         else:
-    #             self.expressionresult = vrange
-
-    # def enterAssignstat(self, ctx: lfrXParser.AssignstatContext):
-    #     self.listermode = ListenerMode.FLUID_ASSIGN_STAT_MODE
-    #     self.expressionresults
-
-    # # TODO: Need to implement the expression parsing
-
-    # def exitAssignstat(self, ctx: lfrXParser.AssignstatContext):
-    #     # TODO: Finish the entire vector implementatinon
-    #     # Check if LHS contains storage, if so figure out what to do
-    #     # perform the vector operations necessary for the what we have here
-    #     # TODO: Evaluate the entire expression an get the final range/concatenation that we want to map
-    #     v = self.expressionresult
-    #     if len(self.lhs) == len(v):
-    #         print("Both sizes are equal")
-    #         # Make 1-1 connections
-    #         for source, target in zip(v, self.lhs):
-    #             print(source, target)
-    #             if isinstance(source, ModuleIO):
-    #                 sourceid = source.name
-    #             else:
-    #                 sourceid = source.id
-
-    #             if isinstance(target, ModuleIO):
-    #                 targetid = target.name
-    #             else:
-    #                 targetid = target.id
-
-    #             self.currentModule.addfluidconnection(sourceid, targetid)
-
-    #     elif len(self.lhs) != len(v):
-    #         print("LHS dim smaller than RHS")
-    #         for source in v:
-    #             if isinstance(source, ModuleIO):
-    #                 sourceid = source.name
-    #             else:
-    #                 sourceid = source.id
-
-    #             for target in self.lhs:
-    #                 if isinstance(target, ModuleIO):
-    #                     targetid = target.name
-    #                 else:
-    #                     targetid = target.id
-
-    #                 self.currentModule.addfluidconnection(sourceid, targetid)
-
-    #     self.listermode = ListenerMode.NONE
-
-    # def exitExpressionterm(self, ctx: lfrXParser.ExpressiontermContext):
-    #     if self.listermode is ListenerMode.FLUID_ASSIGN_STAT_MODE:
-    #         # Add the processed term to the expressionvariable stack
-    #         if ctx.variables() is not None:
-
-    #             print("Expression term is variable")
-    #             name = ctx.variables().vectorvar().ID().getText()
-
-    #             # If the variables is not in the temporary list, add an error
-    #             if name not in self.vectors:
-    #                 self.compilingErrors.append(
-    #                     LFRError(ErrorType.VARIABLE_NOT_RECOGNIZED,
-    #                              "Variable:{0} was not recognized, check to see if this was declared earlier".format(
-    #                                  name)))
-    #                 return
-
-    #             vec = self.vectors[name]
-
-    #             startindex = 0
-    #             endindex = 0
-
-    #             if ctx.variables().vectorvar().vector() is not None:
-    #                 startindex = int(ctx.variables().vectorvar().vector().start.text)
-    #                 endindex = int(ctx.variables().vectorvar().vector().end.text)
-
-    #             vrange = VectorRange(vec, startindex, endindex)
-
-    #             # If theres a unary operator, you need to perform an operation on the thing and add it to the stack
-    #             if ctx.unary_operator() is not None:
-    #                 print("Theres is an unary operator")
-    #                 # TODO:We need to do the unary operator processing on the single vector and put the resultant fluid onto the FIG
-    #                 raise Exception("Need to implement unary operator parsing")
-    #             else:
-    #                 self.expressionvariablestack.append(vrange)
-
-    #         elif ctx.number() is not None:
-
-    #             n = None
-
-    #             try:
-    #                 n = int(ctx.number().getText())
-    #             except ValueError:
-    #                 n = float(ctx.number().getText())
-
-    #             self.expressionvariablestack.append(n)
-
-    # def enterExpression(self, ctx: lfrXParser.ExpressionContext):
-    #     self.expressionvariablestack = []
-    #     self.expressionoperatorstack = []
-
-    # def exitExpression(self, ctx: lfrXParser.ExpressionContext):
-    #     self.expressionvariablestack = None
-    #     self.expressionoperatorstack = None
-
-        # TODO: Evaluate complete expression here
-
-        # for vector in ctx.vector():
-        #     print(vector)
-        #
-        # for ID in ctx.ID():
-        #     name = ID.getText()
-        #     io = self.currentModule.getio(name)
-        #     if io is None:
-        #         self.compilingErrors.append(LFRError(ErrorType.MODULE_IO_NOT_FOUND, " i/o:{0} not declared in module".format(name)))
-        #     else:
-        #         io.type = mode
 
     # def exitTechnologymappingdirective(self, ctx: lfrXParser.TechnologymappingdirectiveContext):
     #     # print("Operator", ctx.operator.getText())
@@ -528,52 +350,7 @@ class LFRCompiler(lfrXListener):
     #     # print(technologystring)
     #     self.operatormap[ctx.operator.getText()] = technologystring
     #
-    # def exitAssignstat(self, ctx: lfrXParser.AssignstatContext):
-    #     # Right now, only the single assignment is available so just work with that
-    #     # Check to see if LHS is temp variable or an io
-    #     ret = self.currentModule.getfluid(ctx.lhs().getText())
-    #     if ret is None:
-    #         #In case we cant find what the LHS is we basically check if its an i/o
-    #         ret = self.currentModule.getio(ctx.lhs().getText())
-    #     print("TEST %:",ctx.lhs().getText(), ret)
-    #     if ret is None:
-    #         raise Exception("Could not assign because we could not map lhs to any known variable")
-    #     elif ret is not None:
-    #         print("LHS: ", ret)
     #
-    #         rvariable = self.expressionvariablestack.pop()
-    #         while len(self.expressionoperatorstack) > 0:
-    #             lvariable = self.expressionvariablestack.pop()
-    #             op = self.expressionoperatorstack.pop()
-    #             # Check if the operator is overridden
-    #             if op in self.operatormap.keys():
-    #                 # Now the Fluid Interaction is overriden
-    #                 overridden_interaction = self.operatormap[op]
-    #                 f1 = self.currentModule.getfluid(rvariable)
-    #                 f2 = self.currentModule.getfluid(lvariable)
-    #                 interaction = self.currentModule.addfluidcustominteraction(f2, f1, overridden_interaction)
-    #                 # Create an edge from the interaction to the output
-    #                 self.currentModule.addinteractionoutput(ret, interaction)
-    #             else:
-    #                 # TODO Figure out how we want to parse the expression, do we want to it here or internally
-    #                 pass
-    #             rvariable = lvariable
-    #
-    #
-    #
-    #     # Final thing to do: clear the operator override
-    #     self.__clearoperatormap()
-    #
-    # def exitExpression(self, ctx: lfrXParser.ExpressionContext):
-    #     # print(ctx.variables())
-    #     # print(ctx.binary_operator())
-    #     # For now just store all the variables and operators and add them from reverse
-    #     variables = [v.getText() for v in ctx.variables()]
-    #     operators = [v.getText() for v in ctx.binary_operator()]
-    #
-    #     # TODO - Split the expression where there are overridden operators
-    #     self.expressionoperatorstack = operators
-    #     self.expressionvariablestack = variables
 
     def exitSkeleton(self, ctx: lfrXParser.SkeletonContext):
         if len(self.compilingErrors) > 0:
--- scripts/runexamples.sh
+++ scripts/runexamples.sh
@@ -7,5 +7,5 @@ for f in $1/*.lfr;
 
 do 
 	echo "Running File $f";
-    python /Users/krishna/CIDAR/pyLFR/cmdline.py --outpath ./out/ $f
+    python /Users/krishna/CIDAR/pyLFR/cmdline.py --outpath ../out/ $f
 done 
