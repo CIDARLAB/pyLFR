1096232e4902854c19c005295af543f95b5f85fa
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2023-03-10 02:52:02 +0000

    Added basic expressions as test cases. Will continue to increase testcases here for compiler coverage. Benchmarks will be used for final designs. But strategic test cases will be placed here.

---
+++ tests/e2e/compiler/test_expressions.py
@@ -0,0 +1,215 @@
+from lfr import api
+from tests.conftest import LIBRARY_PATH, TEST_DATA_FOLDER, TEST_OUTPATH
+
+
+TEST_CASES_FOLDER = TEST_DATA_FOLDER.joinpath("Expressions")
+
+
+def expression1():
+    # expression1.lfr
+    test_file = TEST_CASES_FOLDER.joinpath("expression1.lfr")
+    # Read the text and dump it on console
+    api.compile_lfr(
+        input_files=[str(test_file.absolute())],
+        outpath=str(TEST_OUTPATH),
+        library_path=str(LIBRARY_PATH),
+        no_gen_flag=True,
+        no_annotations_flag=True,
+        pre_load=[],
+    )
+
+
+def expression2():
+    # expression2.lfr
+    test_file = TEST_CASES_FOLDER.joinpath("expression2.lfr")
+    # Read the text and dump it on console
+    api.compile_lfr(
+        input_files=[str(test_file.absolute())],
+        outpath=str(TEST_OUTPATH),
+        library_path=str(LIBRARY_PATH),
+        no_gen_flag=True,
+        no_annotations_flag=True,
+        pre_load=[],
+    )
+
+
+def expression3():
+    # expression3.lfr
+    test_file = TEST_CASES_FOLDER.joinpath("expression3.lfr")
+    # Read the text and dump it on console
+    api.compile_lfr(
+        input_files=[str(test_file.absolute())],
+        outpath=str(TEST_OUTPATH),
+        library_path=str(LIBRARY_PATH),
+        no_gen_flag=True,
+        no_annotations_flag=True,
+        pre_load=[],
+    )
+
+
+def expression4():
+    # expression4.lfr
+    test_file = TEST_CASES_FOLDER.joinpath("expression4.lfr")
+    # Read the text and dump it on console
+    api.compile_lfr(
+        input_files=[str(test_file.absolute())],
+        outpath=str(TEST_OUTPATH),
+        library_path=str(LIBRARY_PATH),
+        no_gen_flag=True,
+        no_annotations_flag=True,
+        pre_load=[],
+    )
+
+
+def expression5():
+    # expression5.lfr
+    test_file = TEST_CASES_FOLDER.joinpath("expression5.lfr")
+    # Read the text and dump it on console
+    api.compile_lfr(
+        input_files=[str(test_file.absolute())],
+        outpath=str(TEST_OUTPATH),
+        library_path=str(LIBRARY_PATH),
+        no_gen_flag=True,
+        no_annotations_flag=True,
+        pre_load=[],
+    )
+
+
+def exporession6():
+    # expression6.lfr
+    test_file = TEST_CASES_FOLDER.joinpath("expression6.lfr")
+    # Read the text and dump it on console
+    api.compile_lfr(
+        input_files=[str(test_file.absolute())],
+        outpath=str(TEST_OUTPATH),
+        library_path=str(LIBRARY_PATH),
+        no_gen_flag=True,
+        no_annotations_flag=True,
+        pre_load=[],
+    )
+
+
+def expression7():
+    # expression7.lfr
+    test_file = TEST_CASES_FOLDER.joinpath("expression7.lfr")
+    # Read the text and dump it on console
+    api.compile_lfr(
+        input_files=[str(test_file.absolute())],
+        outpath=str(TEST_OUTPATH),
+        library_path=str(LIBRARY_PATH),
+        no_gen_flag=True,
+        no_annotations_flag=True,
+        pre_load=[],
+    )
+
+
+def expression8():
+    # expression8.lfr
+    test_file = TEST_CASES_FOLDER.joinpath("expression8.lfr")
+    # Read the text and dump it on console
+    api.compile_lfr(
+        input_files=[str(test_file.absolute())],
+        outpath=str(TEST_OUTPATH),
+        library_path=str(LIBRARY_PATH),
+        no_gen_flag=True,
+        no_annotations_flag=True,
+        pre_load=[],
+    )
+
+
+def expression9():
+    # expression9.lfr
+    test_file = TEST_CASES_FOLDER.joinpath("expression9.lfr")
+    # Read the text and dump it on console
+    api.compile_lfr(
+        input_files=[str(test_file.absolute())],
+        outpath=str(TEST_OUTPATH),
+        library_path=str(LIBRARY_PATH),
+        no_gen_flag=True,
+        no_annotations_flag=True,
+        pre_load=[],
+    )
+
+
+def expression10():
+    # expression10.lfr
+    test_file = TEST_CASES_FOLDER.joinpath("expression10.lfr")
+    # Read the text and dump it on console
+    api.compile_lfr(
+        input_files=[str(test_file.absolute())],
+        outpath=str(TEST_OUTPATH),
+        library_path=str(LIBRARY_PATH),
+        no_gen_flag=True,
+        no_annotations_flag=True,
+        pre_load=[],
+    )
+
+
+def expression11():
+    # expression11.lfr
+    test_file = TEST_CASES_FOLDER.joinpath("expression11.lfr")
+    # Read the text and dump it on console
+    api.compile_lfr(
+        input_files=[str(test_file.absolute())],
+        outpath=str(TEST_OUTPATH),
+        library_path=str(LIBRARY_PATH),
+        no_gen_flag=True,
+        no_annotations_flag=True,
+        pre_load=[],
+    )
+
+
+def expression12():
+    # expression12.lfr
+    test_file = TEST_CASES_FOLDER.joinpath("expression12.lfr")
+    # Read the text and dump it on console
+    api.compile_lfr(
+        input_files=[str(test_file.absolute())],
+        outpath=str(TEST_OUTPATH),
+        library_path=str(LIBRARY_PATH),
+        no_gen_flag=True,
+        no_annotations_flag=True,
+        pre_load=[],
+    )
+
+
+def expression13():
+    # expression13.lfr
+    test_file = TEST_CASES_FOLDER.joinpath("expression13.lfr")
+    # Read the text and dump it on console
+    api.compile_lfr(
+        input_files=[str(test_file.absolute())],
+        outpath=str(TEST_OUTPATH),
+        library_path=str(LIBRARY_PATH),
+        no_gen_flag=True,
+        no_annotations_flag=True,
+        pre_load=[],
+    )
+
+
+def expression14():
+    # expression14.lfr
+    test_file = TEST_CASES_FOLDER.joinpath("expression14.lfr")
+    # Read the text and dump it on console
+    api.compile_lfr(
+        input_files=[str(test_file.absolute())],
+        outpath=str(TEST_OUTPATH),
+        library_path=str(LIBRARY_PATH),
+        no_gen_flag=True,
+        no_annotations_flag=True,
+        pre_load=[],
+    )
+
+
+def expression15():
+    # expression15.lfr
+    test_file = TEST_CASES_FOLDER.joinpath("expression15.lfr")
+    # Read the text and dump it on console
+    api.compile_lfr(
+        input_files=[str(test_file.absolute())],
+        outpath=str(TEST_OUTPATH),
+        library_path=str(LIBRARY_PATH),
+        no_gen_flag=True,
+        no_annotations_flag=True,
+        pre_load=[],
+    )
